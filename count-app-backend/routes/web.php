e0ad930859746b1ba0112211fa52c1f21cc02995
Chamodi Kulathunga <kulathungachamodi@gmail.com>
2023-05-11 11:41:21 +0530

    Created products CRUD
    Created ProductController with products create, index(view all), view(view by id), update, and delete functions.
    Created routes to create, update, delete, viewall, and viewbyid product functions.

--- count-app-backend/routes/web.php
+++ count-app-backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Products Routes
+Route::get('/products/', [ProductController::class, 'index']);
+Route::get('/products/create/', [ProductController::class, 'create']);
+Route::get('/products/{id}/', [ProductController::class, 'view']);
+Route::get('/products/update/{id}', [ProductController::class, 'update']);
+Route::get('/products/delete/{id}', [ProductController::class, 'delete']);
